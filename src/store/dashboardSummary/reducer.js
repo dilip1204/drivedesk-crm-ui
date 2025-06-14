c05c6101ce7c1762151ba97e6d05856abb1abcbe
dilip1204 <dilip1204@users.noreply.github.com>
2025-06-14 22:41:30 +0530

    rajesh changes UI

---
+++ src/store/dashboardSummary/reducer.js
@@ -0,0 +1,38 @@
+import {
+    GET_DASHBOARD_SUMMARY,
+    GET_DASHBOARD_SUMMARY_ERROR,
+    GET_DASHBOARD_SUMMARY_PENDING,
+    GET_DASHBOARD_SUMMARY_SUCCESS
+} from './types';
+
+const initialState = {
+    dashboardSummary: [],
+    dashboardSummaryLoader: false,
+    dashboardSummaryError: [],
+
+}
+
+export default function dashboardSummaryReducer(state = initialState, action) {
+    switch (action.type) {
+        case GET_DASHBOARD_SUMMARY_SUCCESS:
+            return {
+                ...state,
+                dashboardSummary: action.data,
+                dashboardSummaryLoader: false
+            }
+        case GET_DASHBOARD_SUMMARY_PENDING:
+            return {
+                ...state,
+                dashboardSummaryLoader: true
+            }
+        case GET_DASHBOARD_SUMMARY_ERROR:
+            return {
+                ...state,
+                dashboardSummaryError: action.error,
+                dashboardSummaryLoader: false
+            }
+        
+        default:
+            return state;
+    }
+}

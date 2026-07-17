565cc021fffac06eb50782f8977f943662830474
Rajesh <rajeshkumar.wel@gmail.com>
2026-07-17 14:19:10 +0530

    footer text update

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,6 +5,7 @@ import * as Yup from "yup";
 import { useDispatch } from "react-redux";
 import { userLogin } from "../../store/login/actions";
 import logo from "../../assets/logo/logo.png";
+import Footer from "../../components/Footer";
 import "./Login.css";
 
 const Login = () => {
@@ -63,134 +64,137 @@ const Login = () => {
   };
 
   return (
-    <div
-      id="dd-login"
-      className="d-flex align-items-center justify-content-center"
-      style={{ padding: "25px 0" }}
-    >
-      <div className="row justify-content-end" style={{ marginLeft: "250px" }}>
-        <div className="col-lg-6 col-md-10">
-          <div className="card">
-            <div className="card-header bg-primary card-logo-center">
-              <div className="app-brand app-logo">
-                <a href="#">
-                  <img src={logo} alt="logo" />
-                </a>
-              </div>
-            </div>
-
-            <div className="card-body p-5">
-              <h4 className="text-dark mb-5">Sign In</h4>
-
-              {loginError && (
-                <div className="alert alert-danger" role="alert">
-                  {loginError}
+    <div className="dd-login-shell d-flex flex-column min-vh-100">
+      <div
+        id="dd-login"
+        className="d-flex align-items-center justify-content-center flex-grow-1"
+        style={{ padding: "25px 0" }}
+      >
+        <div className="row justify-content-end dd-login-row" style={{ marginLeft: "250px" }}>
+          <div className="col-lg-6 col-md-10">
+            <div className="card">
+              <div className="card-header bg-primary card-logo-center">
+                <div className="app-brand app-logo">
+                  <a href="#">
+                    <img src={logo} alt="logo" />
+                  </a>
                 </div>
-              )}
+              </div>
 
-              <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={onSubmit}
-              >
-                {({ isSubmitting, touched, errors }) => (
-                  <Form>
-                    <div className="row">
-                      {/* Email */}
-                      <div className="form-group col-md-12 mb-4">
-                        <label htmlFor="email" className="form-label">
-                          Email
-                        </label>
-                        <Field
-                          type="email"
-                          name="email"
-                          id="email"
-                          placeholder="Enter your email"
-                          className={`form-control input-lg ${
-                            touched.email
-                              ? errors.email
-                                ? "is-invalid"
-                                : "is-valid"
-                              : ""
-                          }`}
-                        />
-                        <ErrorMessage
-                          name="email"
-                          component="div"
-                          className="invalid-feedback"
-                        />
-                      </div>
+              <div className="card-body p-5">
+                <h4 className="text-dark mb-5">Sign In</h4>
 
-                      {/* Password */}
-                      <div className="form-group col-md-12 mb-4">
-                        <label htmlFor="password" className="form-label">
-                          Password
-                        </label>
-                        <Field
-                          type="password"
-                          name="password"
-                          id="password"
-                          placeholder="Enter your password"
-                          className={`form-control input-lg ${
-                            touched.password
-                              ? errors.password
-                                ? "is-invalid"
-                                : "is-valid"
-                              : ""
-                          }`}
-                        />
-                        <ErrorMessage
-                          name="password"
-                          component="div"
-                          className="invalid-feedback"
-                        />
-                      </div>
+                {loginError && (
+                  <div className="alert alert-danger" role="alert">
+                    {loginError}
+                  </div>
+                )}
 
-                      {/* Remember + Forgot */}
-                      <div className="col-md-12">
-                        <div className="d-flex my-2 justify-content-between">
-                          <div className="form-check">
-                            <input
-                              className="form-check-input"
-                              type="checkbox"
-                              id="rememberMe"
-                            />
-                            <label
-                              className="form-check-label"
-                              htmlFor="rememberMe"
-                            >
-                              Remember me
-                            </label>
-                          </div>
-                          <p>
-                            <a className="text-blue" href="#">
-                              Forgot Your Password?
-                            </a>
-                          </p>
+                <Formik
+                  initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={onSubmit}
+                >
+                  {({ isSubmitting, touched, errors }) => (
+                    <Form>
+                      <div className="row">
+                        {/* Email */}
+                        <div className="form-group col-md-12 mb-4">
+                          <label htmlFor="email" className="form-label">
+                            Email
+                          </label>
+                          <Field
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Enter your email"
+                            className={`form-control input-lg ${
+                              touched.email
+                                ? errors.email
+                                  ? "is-invalid"
+                                  : "is-valid"
+                                : ""
+                            }`}
+                          />
+                          <ErrorMessage
+                            name="email"
+                            component="div"
+                            className="invalid-feedback"
+                          />
                         </div>
 
-                        <button
-                          type="submit"
-                          className="btn btn-lg btn-primary btn-block mb-4 d-flex align-items-center justify-content-center"
-                          disabled={isSubmitting}
-                        >
-                          {isSubmitting ? (
-                            <>
-                              <div className="loader me-2"></div> Signing In...
-                            </>
-                          ) : (
-                            "Sign In"
-                          )}
-                        </button>
+                        {/* Password */}
+                        <div className="form-group col-md-12 mb-4">
+                          <label htmlFor="password" className="form-label">
+                            Password
+                          </label>
+                          <Field
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="Enter your password"
+                            className={`form-control input-lg ${
+                              touched.password
+                                ? errors.password
+                                  ? "is-invalid"
+                                  : "is-valid"
+                                : ""
+                            }`}
+                          />
+                          <ErrorMessage
+                            name="password"
+                            component="div"
+                            className="invalid-feedback"
+                          />
+                        </div>
+
+                        {/* Remember + Forgot */}
+                        <div className="col-md-12">
+                          <div className="d-flex my-2 justify-content-between">
+                            <div className="form-check">
+                              <input
+                                className="form-check-input"
+                                type="checkbox"
+                                id="rememberMe"
+                              />
+                              <label
+                                className="form-check-label"
+                                htmlFor="rememberMe"
+                              >
+                                Remember me
+                              </label>
+                            </div>
+                            <p>
+                              <a className="text-blue" href="#">
+                                Forgot Your Password?
+                              </a>
+                            </p>
+                          </div>
+
+                          <button
+                            type="submit"
+                            className="btn btn-lg btn-primary btn-block mb-4 d-flex align-items-center justify-content-center"
+                            disabled={isSubmitting}
+                          >
+                            {isSubmitting ? (
+                              <>
+                                <div className="loader me-2"></div> Signing In...
+                              </>
+                            ) : (
+                              "Sign In"
+                            )}
+                          </button>
+                        </div>
                       </div>
-                    </div>
-                  </Form>
-                )}
-              </Formik>
+                    </Form>
+                  )}
+                </Formik>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };

04317259d55496f4b53b137518efce0cf0c6d778
dilip1204 <dilip1204@users.noreply.github.com>
2026-08-08 13:54:23 +0530

    updating expense

--- src/services/functional/expenses/getExpenseSummaryService.js
+++ src/services/functional/expenses/getExpenseSummaryService.js
@@ -2,10 +2,12 @@ import { METHOD_TYPES } from "../../../shared/constants/methodTypes";
 import { request } from "../../core/networkRequest";
 import { ENDPOINTS } from "../../../shared/constants/endPoints";
 
-export const getExpenseSummary = ({ fromDate, toDate }) => {
+export const getExpenseSummary = ({ fromDate, toDate, skip, limit }) => {
   const params = new URLSearchParams({
     from_date: fromDate,
     to_date: toDate,
+    skip: String(skip),
+    limit: String(limit),
   });
 
   return request(METHOD_TYPES.GET, `${ENDPOINTS.getExpenseSummary}?${params.toString()}`);
